ccc62d00d98fab04521f02af6c51126eee714e34
pleasant-calculator-7755 <domcode88@gmail.com>
2020-08-18 15:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyCustomHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  `It's ${days} days until ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`;
 }
